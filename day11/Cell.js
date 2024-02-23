3b724699719267b0b548181b8e974dd3d972ad45
gieeun <gieun0528@gamil.com>
2024-02-23 11:49:51 +0900

    -금일 수업

---
+++ day11/Cell.js
@@ -0,0 +1,35 @@
+/*
+달력의 셀을 정의
+*/
+class Cell{
+    constructor(container, x, y, width, height, content){
+        this.container=container;
+        this.div=document.createElement("div");
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.content=content; //셀에 채워질 내용
+
+        //style 
+        this.div.style.border="1px solid gray";
+        this.div.style.position="absolute";
+        this.div.style.left=this.x+"px";
+        this.div.style.top=this.y+"px";
+
+        this.div.style.width=this.width+"px";
+        this.div.style.height=this.height+"px";
+
+        //셀에 표시될 텍스트 정보 
+        this.div.innerText = this.content;
+        this.container.appendChild(this.div);
+    }
+
+    setContent(content){
+        //물리량 변화 
+        this.content=content; 
+
+        //렌더링
+        this.div.innerText=this.content;
+    }
+}
